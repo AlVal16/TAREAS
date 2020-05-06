44f4f7f370313f3e45ffa77f24b8bbcfecd1b279
unknown <you@example.com>
2020-05-05 20:24:34 -0500

    Nulo

---
+++ Cla5/src/Nlo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Nlo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		           int a; 
+				
+				Scanner scan=new Scanner (System.in);
+				 System.out.println("Escribir número");
+				 a=scan.nextInt();
+				
+				 if(a<0) {
+						//verdadera
+						System.out.println("Negativo");
+				 }   
+				
+				 else {
+						System.out.println("Positivo");
+						 } if (a== 0) {
+						 System.out.println("Nulo ");
+						 }
+						 }
+			}
+
+
+
